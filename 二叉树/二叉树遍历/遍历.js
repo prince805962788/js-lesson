c72d3f5094da6a08b7966e1df7c8821b9771061c
wangziqian <wzqwangziqian_v@didichuxing.com>
2020-07-09 19:28:06 +0800

    二叉树和map

---
+++ 二叉树/二叉树遍历/遍历.js
@@ -0,0 +1,48 @@
+const root = {
+	val: 'A',
+	left: {
+		val: 'B',
+		left: {
+			val: 'D'
+		},
+		right: {
+			val: 'E'
+		}
+	},
+	right: {
+		val: 'C',
+		right: {
+			val: 'F'
+		}
+	}
+}
+// 先序遍历
+const preorder = (root) => {
+	if(!root) {
+		return 
+	}
+	console.log('先序',root.val)
+	preorder(root.left)
+	preorder(root.right)
+}
+preorder(root)
+//中序遍历
+const inorder = (root) => {
+	if(!root){
+		return
+	}
+	inorder(root.left)
+	console.log('中序',root.val)
+	inorder(root.right)
+}
+inorder(root)
+//后序遍历
+const postorder = (root) => {
+	if(!root) {
+		return 
+	}
+	postorder(root.left)  
+	postorder(root.right)
+	console.log('后序', root.val)  
+}
+postorder(root)
