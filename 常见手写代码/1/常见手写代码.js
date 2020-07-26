290bfeb5569aaeebb9f80d0e302d04ab7cc50a50
PrinceLee <prince805962788@163.com>
2020-07-26 14:54:09 +0800

    常见手写代码

---
+++ 常见手写代码/1/常见手写代码.js
@@ -0,0 +1,76 @@
+//new的实现
+function New(func) {
+  const res = {}
+  if (func.prototype !== null) { //如果构造函数不为空
+    res.__proto__ = func.prototype //实力的原形对象指向构造函数的原形
+  }
+  const args = Array.prototype.slice.call(arguments, 1) //取出除去构造函数的剩余参数
+  const funcRes = func.apply(res, args) //使用构造函数func在res作用域下执行
+  //如果函数没有返回对象类型Object(包含Functoin, Array, Date, RegExg, Error)，那么new表达式中的函数调用会自动返回这个新的对象。
+  if (typeof funcRes === 'object' || typeof funcRes === 'null' || typeof funcRes === 'function') {
+    return funcRes
+  } else {
+    return res
+  }
+}
+console.log('--------New的实现--------')
+
+function setObj(name, gender) {
+  this.name = name
+  this.gender = gender
+}
+const res1 = new setObj('lee', 'male')
+const res2 = New(setObj, 'lee', 'male')
+console.log(res1)
+console.log(res2)
+
+//call的实现
+Function.prototype.myCall = function (context = window) {
+  context.fn = this
+  const arg = [...arguments].slice(1)
+  const result = context.fn(...arg)
+  delete context.fn
+  return result
+}
+//apply的实现
+Function.prototype.myApply = function (context = window) {
+  context.fn = this
+  let result
+  if (arguments.length >= 1) {
+    result = context.fn(...arguments[1])
+  } else {
+    result = context.fn()
+  }
+  delete context.fn
+  return result
+}
+//bind的实现
+Function.prototype.myBind = function (context = window) {
+  if (typeof this != "function") {
+    throw Error("not a function")
+  }
+  let fn = this
+  const args = [...arguments].slice(1) // Array.prototype.slice.call(arguments, 1)
+  let resFn = function () {
+    return fn.apply(this instanceof resFn ? this : context, args.concat(...arguments))
+  }
+  resFn.prototype = this.prototype
+  return resFn
+}
+const obj = {
+  value: 123
+}
+const testCall = function (name, age) {
+  console.log('myCall:', name, age, this.value)
+}
+const testApply = function (name, age) {
+  console.log('myApply:', name, age, this.value)
+}
+const testBind = function (name, age) {
+  console.log('myBind:', name, age, this.value)
+}
+console.log('--------call,apply,bind的实现--------')
+testCall.myCall(obj, 'lee', 24)
+testApply.myApply(obj, ['prince', 25])
+let bind = testBind.myBind(obj, 'princlee', 26)
+bind()
---
+++ 常见手写代码/2/常见手写代码2.js
@@ -0,0 +1,58 @@
+//寄生组合式继承
+function Parent(name) {
+  this.name = name
+}
+
+function Child(name, age) {
+  Parent.call(this, name)
+  this.age = age
+}
+Child.prototype = Object.create(Parent.prototype)
+Child.prototype.constructor = Child
+let parent = new Parent('lee')
+let child = new Child('prince', 23)
+console.log(parent, child, child instanceof Parent)
+//数组扁平化
+function platArr(arr) {
+  const res = []
+  const plat = arr => {
+    arr.map(item => {
+      Array.isArray(item) ? plat(item) : res.push(item)
+    })
+  }
+  plat(arr)
+  return res
+}
+const arr = [1, 2, [3, 4, [5, 6]], 7, 8]
+console.log('------------数组扁平化------------')
+console.log(platArr(arr))
+//函数柯里化
+function curry(fn, args) {
+  const length = fn.length //函数fn的参数数量
+  args = args || []
+  return function () {
+    const newArgs = args.concat(Array.prototype.slice.call(arguments))
+    if (newArgs.length < length) {
+      return curry.call(this, fn, newArgs)
+    } else {
+      return fn.apply(this, newArgs);
+    }
+  }
+}
+
+function multiFn(a, b, c) {
+  return a * b * c;
+}
+
+const multi = curry(multiFn);
+multi(2)(3)(4);
+//实现一个intanceof
+function intanceof(l, r) {
+  const left = l.__proto
+  const right = r.prototype
+  while (true) {
+    if (left === null) return false
+    if (left === right) return true
+    left = left.__proto__
+  }
+}
