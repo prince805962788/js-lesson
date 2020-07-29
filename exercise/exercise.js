2e328f1071442aca958a146f972489495e51cee8
wangziqian <wzqwangziqian_v@didichuxing.com>
2020-07-29 16:52:40 +0800

    实现一个reduce

--- exercise/exercise.js
+++ exercise/exercise.js
@@ -1,16 +1,15 @@
-class Node {
-  constructor(data) {
-    this.value = data
-    this.next = null
-  }
+//实现一个reduce
+Array.prototype.myReduce = function(func, initialValue) {
+  let arr = this
+  let base = typeof initialValue === 'undefined' ? arr[0] : initialValue
+  let startPoint = typeof initialValue === 'undefined' ? 1 : 0
+  arr.slice(startPoint)
+      .forEach((val, index)=> {
+          base = func(base, val, index + startPoint, arr)
+      })
+  return base
 }
-class NodeList {
-  constructor(arr) {
-    const head = new Node(arr.shift())
-    const next = head
-    arr.map(item => {
-      next.next = new Node(item)
-      next = next.next
-    })
-  }
-}
+let res = [1,2,3,4].myReduce((prev,cur)=>{
+	return prev+cur
+})
+console.log(res)
--- 常见手写代码/3/常见手写代码.js
+++ 常见手写代码/3/常见手写代码.js
@@ -28,3 +28,15 @@ var debounceRun = throttle(function () {
 }, 2000)
 // 只有当鼠标移动停止后2s打印123
 window.addEventListener('mousemove', debounceRun)
+
+//实现一个reduce
+Array.prototype.myReduce = function(func, initialValue) {
+  var arr = this
+  var base = typeof initialValue === 'undefined' ? arr[0] : initialValue
+  var startPoint = typeof initialValue === 'undefined' ? 1 : 0
+  arr.slice(startPoint)
+      .forEach(function(val, index) {
+          base = func(base, val, index + startPoint, arr)
+      })
+  return base
+}
