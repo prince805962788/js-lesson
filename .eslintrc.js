59a5744126cd033f861875e2758048c3e61f5b6d
PrinceLee <prince805962788@163.com>
2020-11-15 20:43:55 +0800

    eslint修改

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,35 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": "eslint:recommended",
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    rules: {
-      "block-spacing": 2,
-      "comma-spacing": ["error", { "before": false, "after": true }],
-      "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
-      "no-mixed-spaces-and-tabs": "error",
-      "semi-spacing": ["error", { "before": false, "after": true }],
-      "space-before-blocks": ["error", "always"],
-      "space-before-function-paren": ["error", "always"],
-      "space-unary-ops": ["error", { "words": true, "nonwords": false }],
-      "space-infix-ops": "error",
-      "arrow-spacing": 1,
-      "indent": [2, 2, {SwitchCase: 1}],
-      "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    }
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: "eslint:recommended",
+  globals: {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  parserOptions: {
+    "parser": "babel-eslint",
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+  },
+  rules: {
+    "strict": 0,
+    "block-spacing": 2,
+    "comma-spacing": ["error", { "before": false, "after": true }],
+    "key-spacing": ["error", { "beforeColon": false, "afterColon": true }],
+    "no-mixed-spaces-and-tabs": "error",
+    "semi-spacing": ["error", { "before": false, "after": true }],
+    "space-before-blocks": ["error", "always"],
+    "space-before-function-paren": ["error", "always"],
+    "space-unary-ops": ["error", { "words": true, "nonwords": false }],
+    "space-infix-ops": "error",
+    "arrow-spacing": 1,
+    "indent": [2, 2, {SwitchCase: 1}],
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+  }
   
 };
